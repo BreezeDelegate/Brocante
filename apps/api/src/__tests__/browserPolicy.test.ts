5063a0dd615b3158fc760abe4571bbc3d8970e13
Breeze <joeabysseyes+2@gmail.com>
2026-08-12 11:57:33 +0200

    Expand browser egress tests

--- apps/api/src/__tests__/browserPolicy.test.ts
+++ apps/api/src/__tests__/browserPolicy.test.ts
@@ -6,12 +6,20 @@ describe('browser network policy', () => {
   it.each([
     'http://127.0.0.1:8787/',
     'http://10.0.0.1/',
+    'http://100.64.0.1/',
     'http://169.254.169.254/latest/meta-data/',
+    'http://172.16.0.1/',
     'http://192.168.1.20/',
+    'http://192.0.2.1/',
+    'http://198.51.100.1/',
+    'http://203.0.113.1/',
     'http://host.docker.internal:11434/',
+    'http://ollama.internal/',
+    'http://service/',
     'http://[::1]/',
     'http://[fd00::1]/',
-  ])('blocks private target %s', (url) => {
+    'http://[fe80::1]/',
+  ])('blocks private, local or non-routable target %s', (url) => {
     expect(isBlockedBrowserUrl(url)).toBe(true);
   });
 
